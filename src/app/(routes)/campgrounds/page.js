8e05574e7d8be9429b1c7e74cf530179de68bad1
Nandagopal <168453056+1nandagopal@users.noreply.github.com>
2024-10-01 00:00:23 +0530

    Link to individual campground page

--- src/app/(routes)/campgrounds/page.js
+++ src/app/(routes)/campgrounds/page.js
@@ -6,6 +6,7 @@ import {
   CardHeader,
   Image,
 } from "@nextui-org/react";
+import Link from "next/link";
 
 export default async function AllCampgroundsPage() {
   const campgrounds = await getAllCampgrounds();
@@ -13,26 +14,31 @@ export default async function AllCampgroundsPage() {
   return (
     <div className="flex flex-wrap gap-4">
       {campgrounds.map((campground) => (
-        <Card className="max-w-[350px] p-1" key={campground.id}>
-          <CardHeader>
-            <div className="flex justify-between items-center text-lg font-semibold w-full">
-              <h3>{campground.title}</h3>
-              <p>${campground.price}</p>
-            </div>
-          </CardHeader>
-          <CardBody className="space-y-2 p-0 px-3">
-            <Image
-              src={campground.images[0]}
-              width={300}
-              height={250}
-              className="object-cover"
-            />
-            <p>📌 {campground.location}</p>
-          </CardBody>
-          <CardFooter className="text-gray-500 text-sm">
-            By {campground.author.name}
-          </CardFooter>
-        </Card>
+        <Link href={`/${campground.id}`} key={campground.id}>
+          <Card
+            className="max-w-[350px] p-1 hover:scale-[1.02]"
+            key={campground.id}
+          >
+            <CardHeader>
+              <div className="flex justify-between items-center text-lg font-semibold w-full">
+                <h3>{campground.title}</h3>
+                <p>${campground.price}</p>
+              </div>
+            </CardHeader>
+            <CardBody className="space-y-2 p-0 px-3">
+              <Image
+                src={campground.images[0]}
+                width={300}
+                height={250}
+                className="object-cover"
+              />
+              <p>📌 {campground.location}</p>
+            </CardBody>
+            <CardFooter className="text-gray-500 text-sm">
+              By {campground.author.name}
+            </CardFooter>
+          </Card>
+        </Link>
       ))}
     </div>
   );
