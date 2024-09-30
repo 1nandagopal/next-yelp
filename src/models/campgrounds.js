29bae395d7cabd1f27cea12ed3ecd375b1d5ad50
Nandagopal <168453056+1nandagopal@users.noreply.github.com>
2024-09-30 18:13:54 +0530

    Update campground schema to include User ref

--- src/models/campgrounds.js
+++ src/models/campgrounds.js
@@ -1,11 +1,34 @@
 import mongoose from "mongoose";
+import { User } from "./users";
 
 const CampgroundsSchema = new mongoose.Schema({
-  title: String,
-  price: Number,
-  description: String,
-  location: String,
-  images: [String],
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  images: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  author: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 export const Campground =
