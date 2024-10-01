49448631e0f47c5939aac736cd350982dfefa4e4
Nandagopal <168453056+1nandagopal@users.noreply.github.com>
2024-10-01 22:39:20 +0530

    Added basic page for individual campground

--- src/app/(routes)/[id]/page.js
+++ src/app/(routes)/[id]/page.js
@@ -1,25 +1,29 @@
 import { getCampground } from "@/actions/campgrounds";
 import { ImageCarousel } from "@/components/image-carousel";
+import { ScrollShadow } from "@nextui-org/react";
 
 export default async function CampgroundPage({ params }) {
   const campground = await getCampground(params.id);
-  console.log(campground);
+
+  if (!campground) return;
 
   return (
-    <div className="p-6 h-full">
-      <div className="grid grid-cols-2 gap-4 h-full">
-        <ImageCarousel images={campground.images} />
-        <div>
-          <div className="flex justify-between font-semibold">
-            <h3 className="text-2xl">{campground.title}</h3>
-            <p>${campground.price}/night</p>
-          </div>
-          <p>{campground.description}</p>
-          <p>{campground.location}</p>
-          <p>By {campground.author.name}</p>
+    <div className="grid grid-cols-3 grid-rows-2 gap-4 h-full">
+      <ImageCarousel images={campground.images} />
+      <div className="h-full col-span-2 flex flex-col space-y-2">
+        <div className="flex justify-between items-center font-semibold">
+          <h3 className="text-2xl">{campground.title}</h3>
+          <p>${campground.price}/night</p>
         </div>
-        <div></div>
-        <div></div>
+        <ScrollShadow className="">{campground.description}</ScrollShadow>
+        <p>{campground.location}</p>
+        <p>By {campground.author.name}</p>
+      </div>
+      <div className="bg-gray-400 h-full flex justify-center items-center">
+        Map place holder
+      </div>
+      <div className="bg-rose-400 h-full flex justify-center items-center">
+        D
       </div>
     </div>
   );
