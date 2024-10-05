6ad6395786ec1ea51854bf0841f512ee4ddf50a5
Nandagopal <168453056+1nandagopal@users.noreply.github.com>
2024-10-05 15:54:49 +0530

    Added drop down options

--- src/app/(routes)/mycampgrounds/page.js
+++ src/app/(routes)/mycampgrounds/page.js
@@ -1,6 +1,16 @@
 import actions from "@/actions";
 import { auth } from "@/libs/auth";
-import { Card, CardBody, CardHeader, Image } from "@nextui-org/react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Image,
+  Button,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import Link from "next/link";
 
 export default async function MyCampgroundsPage() {
   const session = await auth();
@@ -13,10 +23,30 @@ export default async function MyCampgroundsPage() {
       <h2 className="text-2xl">My Campgrounds</h2>
       <div className="my-6 grid grid-cols-3 gap-2">
         {campgrounds.map((campground) => (
-          <Card key={campground.id} isPressable>
+          <Card key={campground.id}>
             <CardHeader>
               <div className="flex items-center justify-between w-full">
                 <h3>{campground.title}</h3>
+                <Popover radius="sm" placement="bottom">
+                  <PopoverTrigger>
+                    <Button isIconOnly>+</Button>
+                  </PopoverTrigger>
+                  <PopoverContent className="p-0 overflow-hidden">
+                    <Link href={`/${campground.id}`}>
+                      <Button variant="light" radius="none">
+                        Visit
+                      </Button>
+                    </Link>
+                    <Link href={`/${campground.id}/edit`}>
+                      <Button color="warning" variant="light" radius="none">
+                        Edit
+                      </Button>
+                    </Link>
+                    <Button color="danger" variant="light" radius="none">
+                      Delete
+                    </Button>
+                  </PopoverContent>
+                </Popover>
               </div>
             </CardHeader>
             <CardBody>
