72239d6d257911b108e10c7c517e2b54451ac65e
Nandagopal <168453056+1nandagopal@users.noreply.github.com>
2024-10-08 17:39:22 +0530

    Improved /new

--- src/app/(routes)/new/page.js
+++ src/app/(routes)/new/page.js
@@ -1,9 +1,14 @@
 import { NewCampgroundForm } from "@/components/new/new-form";
+import { auth } from "@/libs/auth";
+import { redirect } from "next/navigation";
 
-export default function NewCampgroundPage() {
+export default async function NewCampgroundPage() {
+  const session = await auth();
+  if (!session?.user) redirect("/auth");
+
   return (
     <div className="max-w-3xl mx-auto">
-      <h2 className="my-8 text-2xl">New campground</h2>
+      <h2 className="my-4 text-2xl font-medium">New campground</h2>
       <NewCampgroundForm />
     </div>
   );
